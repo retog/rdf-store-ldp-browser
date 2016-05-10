72857b96330342a2f54a225373ae7f5e163d5993
Reto Gmuer <reto@apache.org>
2016-05-10 13:51:17 +0000

    prepared to use 0.3.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var LdpStore = require('rdf-store-ldp');
-var MimeTypeUtil = require('rdf-mime-type-util');
-LdpStore.ParserUtil = MimeTypeUtil.ParserUtil;
-LdpStore.parsers = MimeTypeUtil.parsers;
+//var MimeTypeUtil = require('rdf-mime-type-util');
+//LdpStore.ParserUtil = MimeTypeUtil.ParserUtil;
+//LdpStore.parsers = MimeTypeUtil.parsers;
 if (typeof window !== 'undefined') {
     window.LdpStore = LdpStore;
 }
